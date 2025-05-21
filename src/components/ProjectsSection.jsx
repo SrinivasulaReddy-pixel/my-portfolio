cf3b59c1ed11dd81bb74804cf0ce64d12ba510c6
1217839 <balusrinivasulareddy@gmail.com>
2025-05-21 18:25:19 -0400

    added one project in project sections

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,10 +3,10 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
+    title: "My Portfolio",
+    description: "A beautiful portfolio app using React and Tailwind.",
     image: "/projects/project1.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
+    tags: ["React", "TailwindCSS", "Javascript"],
     demoUrl: "#",
     githubUrl: "#",
   },
